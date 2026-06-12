0e2a7188ee10f051ccf19bed29ce8a98ef80f5d5
Regina Aranda <lukanavat@gmail.com>
2026-06-12 14:20:48 -0600

    Subida de python de integrales

---
+++ CW08/Numerical_Integration.py
@@ -0,0 +1,51 @@
+import math
+
+#INPUT
+a=input("Write the left endpoint of the interval: ")
+b=input("Write the right endpoint of the interval: ")
+f_x=input("Write the functtion to integrate: ")
+method=input("Writhe the integration method (LRM,RRM,MRM,TRAP)")
+
+
+#PROCESS
+if "pi" in a:
+    a = eval(a.replace("pi", str(math.pi)))
+else:
+    a = float(a)
+
+if "pi" in b:
+    b = eval(b.replace("pi", str(math.pi)))
+else:
+    b = float(b)
+
+n=1000
+h=(b-a)/n
+area=0.0
+shift=0
+constant=0
+variable=0
+
+
+if method=="RRM":
+    shift=1
+if method=="MRM":
+    constant= h/2
+if method=="TRAP":
+    variable=1
+    f_0=f_x.replace("x",str(a))
+    area += (h/2)* eval(f_0)
+    
+    for i in range (variable, n):
+        xi = a + i * h
+        f_xi = f_x.replace ("x", str(xi))
+        area += (h/2) * 2 * eval(f_xi)
+    f_xn=f_x.replace("x",str(b))
+    area += (h/2) * eval(f_xn)
+else:   
+    for  i in range (0 + shift,n + shift):
+        xi = a + i * h
+        height=f_x.replace("x", str(xi + constant))
+        area +=h * eval (height)
+
+#DISPLAY 
+print(f"The integration of {f_x} is {area}")
